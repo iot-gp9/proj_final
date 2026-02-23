b6536665d36fd61c023204f387708ff9b5f13c71
Akira - Cleber Akira Nakandakare <cakira@cpqd.com.br>
2026-02-23 18:12:39 -0300

    Trocamos o certificado ROOT pelo certificado do próprio Ubidots
    O objetivo disso é tentar melhorar a performance durante a conexão

--- src/main.cpp
+++ src/main.cpp
@@ -23,41 +23,38 @@
 #define PIN_TRIGGER 25
 #define PIN_ECHO 34
 
-/* 
+/*
  * CERTIFICADO UBIDOTS (ISRG Root X1)
- * Válido até 04 Jun 2035
- */ 
+ * Válido até 04 Jun 2035 (correção, ver abaixo)
+ *
+ * ***********
+ * IMPORTANTE: Tentando aumentar a performance, trocamos o certificado Root
+ * pelo certificado do Ubidots, que é valido somente até 18 abr 2026.
+ */
 const char* UBIDOTS_ROOT_CA = \
   "-----BEGIN CERTIFICATE-----\n" \
-  "MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw\n" \
-  "TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh\n" \
-  "cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4\n" \
-  "WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu\n" \
-  "ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY\n" \
-  "MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc\n" \
-  "h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+\n" \
-  "0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U\n" \
-  "A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW\n" \
-  "T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH\n" \
-  "B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC\n" \
-  "B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv\n" \
-  "KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn\n" \
-  "OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn\n" \
-  "jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw\n" \
-  "qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI\n" \
-  "rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV\n" \
-  "HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq\n" \
-  "hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL\n" \
-  "ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ\n" \
-  "3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK\n" \
-  "NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5\n" \
-  "ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur\n" \
-  "TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC\n" \
-  "jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc\n" \
-  "oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq\n" \
-  "4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA\n" \
-  "mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d\n" \
-  "emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=\n" \
+  "MIID/jCCA4OgAwIBAgISBaefSmooirx6XyayYj0OCvZaMAoGCCqGSM49BAMDMDIx\n" \
+  "CzAJBgNVBAYTAlVTMRYwFAYDVQQKEw1MZXQncyBFbmNyeXB0MQswCQYDVQQDEwJF\n" \
+  "ODAeFw0yNjAxMTgyMzI3MzVaFw0yNjA0MTgyMzI3MzRaMCExHzAdBgNVBAMTFmlu\n" \
+  "ZHVzdHJpYWwudWJpZG90cy5jb20wWTATBgcqhkjOPQIBBggqhkjOPQMBBwNCAAR7\n" \
+  "QF4rWhRPTpOzlvDn1AKF/X2Q/TlcOyw5pXI9e3c2rQFKbStEB0cnL0KxPt4Q/khY\n" \
+  "PHNxfqlS6eAVv/LF6X/zo4ICiDCCAoQwDgYDVR0PAQH/BAQDAgeAMB0GA1UdJQQW\n" \
+  "MBQGCCsGAQUFBwMBBggrBgEFBQcDAjAMBgNVHRMBAf8EAjAAMB0GA1UdDgQWBBRV\n" \
+  "5t0w+poRe6DXjekmiKoNEAMotDAfBgNVHSMEGDAWgBSPDROi9i5+0VBsMxg4XVmO\n" \
+  "I3KRyjAyBggrBgEFBQcBAQQmMCQwIgYIKwYBBQUHMAKGFmh0dHA6Ly9lOC5pLmxl\n" \
+  "bmNyLm9yZy8wgYUGA1UdEQR+MHyCD2FwcC51Ymlkb3RzLmNvbYIQYXBwMi51Ymlk\n" \
+  "b3RzLmNvbYIUYnVzaW5lc3MudWJpZG90cy5jb22CFmluZHVzdHJpYWwudWJpZG90\n" \
+  "cy5jb22CEHN0ZW0udWJpZG90cy5jb22CF3ViaWRvdHMuaW90LnViaWRvdHMuY29t\n" \
+  "MBMGA1UdIAQMMAowCAYGZ4EMAQIBMC0GA1UdHwQmMCQwIqAgoB6GHGh0dHA6Ly9l\n" \
+  "OC5jLmxlbmNyLm9yZy85MS5jcmwwggEDBgorBgEEAdZ5AgQCBIH0BIHxAO8AdgDL\n" \
+  "OPcViXyEoURfW8Hd+8lu8ppZzUcKaQWFsMsUwxRY5wAAAZvTpQRIAAAEAwBHMEUC\n" \
+  "IQDr/KUvDUspRrowwqWaVTeskcWgbI2XCqW+/FSYOyCcmgIgMfsm0PY5+3A6AESU\n" \
+  "segq33koyC+pBX5U9Vq2PrCXcJYAdQAOV5S8866pPjMbLJkHs/eQ35vCPXEyJd0h\n" \
+  "qSWsYcVOIQAAAZvTpQQ2AAAEAwBGMEQCIDzJrisa6+H5YbPWC2iHGO7eQ8AcHre/\n" \
+  "uN2eDublWuVDAiBQTLVRoQnv9J3n5piqerdXlo06HK9Lzb0Q5CRx4aJPAzAKBggq\n" \
+  "hkjOPQQDAwNpADBmAjEA2fmDMV60lcSCdGe70M0PLX5L6Tjz24Qpn3vsncYrULH+\n" \
+  "T60vFZ7fQJO9PiI0yCEcAjEAlvTBIKS9PzH2HC3ypHtzgfGD7adNCZTqHjZmRiHV\n" \
+  "jJKX5/ng6+4JHtBTVUg8NxGy\n" \
   "-----END CERTIFICATE-----\n";
 
 /*
